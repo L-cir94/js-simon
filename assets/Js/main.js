07f041240a061fa7d083006d1987be98070bf32e
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-21 15:56:46 +0100

    generate 5 numbers in page

---
+++ assets/Js/main.js
@@ -0,0 +1,37 @@
+
+/* 
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, 
+i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri 
+da indovinare sono stati individuati.
+Consigli del giorno:
+Pensate prima in italiano.
+Dividete in piccoli problemi la consegna.
+Individuate gli elementi di cui avete bisogno per realizzare il programma.
+Bonus:
+Invece di usare prompt e allerte usate inputs ed elementi della dom per mostrare a schermo 
+il risultato.
+*/
+
+/* strumenti: 
+
+*/
+
+//Visualizzare in pagina 5 numeri casuali.
+const resultElement = document.getElementById('result')
+const pcNumb = [];
+
+function generaNumCasuali(min, max) {
+  let randNum
+  for (let i = 0; i < max; i++) {
+    randNum = Math.floor(Math.random() * (max - min + 1)) + min ;
+    resultElement.append(randNum);
+    pcNumb.push(randNum)
+  }
+  return randNum
+}
+
+generaNumCasuali(1,5)
+console.log(pcNumb);
+//Da lì parte un timer di 30 secondi.
